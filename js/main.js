4a57f96e0f71b1ad188bcc1eb268c4f84b331372
Christopher Klein <chris.klein@t-online.de>
2019-09-28 20:26:49 +0200

    form: now finally in a quite good state

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-;(function () {
+;(function() {
 	
 	'use strict';
+
+	$("#age").selectmenu();
 
 	// iPad and iPod detection	
 	var isiPad = function(){
@@ -29,8 +31,7 @@
 		});
 	};
 
-	var sliderMain = function() {
-		
+	var sliderMain = function() {	
 	  	$('#qbootstrap-slider-hero .flexslider').flexslider({
 			animation: "fade",
 			slideshowSpeed: 10000,
@@ -94,7 +95,6 @@
 
 		  	var slider = $('#qbootstrap-slider-responsum .flexslider').data("flexslider");
 
-
 			const FORM_MAX_PEOPLE = 4;
 
 			var slides = []
@@ -102,11 +102,9 @@
 				slides.push(false);
 			}
 
-
 			/**
 			 * ADD
 			 */
-
 
 			// add  person
 			$("#attendee-add").click(function() {
@@ -120,7 +118,6 @@
 					alert("Maximum erreicht!");
 					return;		
 				}	
-
 				// https://stackoverflow.com/questions/27542147/dynamic-add-slide-in-flexslider-carousel
 				// add the line
 				slider.addSlide($('<li>' + buildFormPerson(first) + '</li>'));	
@@ -129,7 +126,6 @@
 			function buildFormPerson(first) {		
 				var btnType = first ? "add" : "sub";
 				var btnVal = first ? "Hinzufügen" : "Entfernen";
-
 				//var persNo = getTotalSlideCount() + 1;				
 				function getFreePersNo() {
 					if (slides.length > 0) {
@@ -144,7 +140,6 @@
 				}
 
 				var persNo = getFreePersNo() + 1;
-
 				return `
 					<div class="col-md-10 form-group form-person">
 						<div class="row">
@@ -208,7 +203,6 @@
 				removeFormPerson(); 
 			});	
 
-
 			// remove form line on sub-button click
 			function removeFormPerson(index) {				
 				//$("#attend-form-line-" + index).remove();		
@@ -218,10 +212,8 @@
 			}
 
 			// first form line
-			addFormPerson(true);
+			//addFormPerson(true);
 			
-
-
 
 			// submit logic
 
@@ -301,14 +293,13 @@
 
 	// Parallax
 	var parallax = function() {
-		if ( !isiPad() || !isiPhone() ) {
+		if (!isiPad() || !isiPhone()) {
 			$(window).stellar();
 		}
 	};
 
 	// Page Nav
 	var clickMenu = function() {
-
 		$('a:not([class="external"])').click(function(event){
 			var section = $(this).data('nav-section'),
 				navbar = $('#navbar');
@@ -325,23 +316,18 @@
 		    event.preventDefault();
 		    return false;
 		});
-
 	};
 
 	// Reflect scrolling in navigation
 	var navActive = function(section) {
-
 		var $el = $('#navbar > ul');
 		$el.find('li').removeClass('active');
 		$el.each(function(){
 			$(this).find('a[data-nav-section="'+section+'"]').closest('li').addClass('active');
 		});
-
 	};
 	var navigationSection = function() {
-
 		var $section = $('div[data-section]');
-		
 		$section.waypoint(function(direction) {
 		  	if (direction === 'down') {
 		    	navActive($(this.element).data('section'));
@@ -358,20 +344,16 @@
 		}, {
 		  	offset: function() { return -$(this.element).height() + 155; }
 		});
-
 	};
 
 
 	// Window Scroll
 	var windowScroll = function() {
 		var lastScrollTop = 0;
-
 		$(window).scroll(function(event){
-
 		   	var header = $('#qbootstrap-header'),
 				scrlTop = $(this).scrollTop();
-
-			if ( scrlTop > 500 && scrlTop <= 2000 ) {
+			if (scrlTop > 500 && scrlTop <= 2000) {
 				header.addClass('navbar-fixed-top qbootstrap-animated slideInDown');
 			} else if ( scrlTop <= 500) {
 				if ( header.hasClass('navbar-fixed-top') ) {
@@ -385,20 +367,14 @@
 		});
 	};
 
-
-
 	// Animations
 	var contentWayPoint = function() {
 		var i = 0;
 		$('.animate-box').waypoint( function( direction ) {
-
 			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
 				i++;
-
 				$(this.element).addClass('item-animate');
 				setTimeout(function(){
-
 					$('body .animate-box.item-animate').each(function(k){
 						var el = $(this);
 						setTimeout( function () {
@@ -412,111 +388,93 @@
 							} else {
 								el.addClass('fadeInUp animated');
 							}
-
 							el.removeClass('item-animate');
 						},  k * 50, 'easeInOutExpo' );
 					});
-					
-				}, 50);
-				
+				}, 50);	
 			}
-
-		} , { offset: '85%' } );
+		}, { offset: '85%' } );
 	};
 
-
 	var inlineSVG = function() {
-		$('img.svg').each(function(){
-	    var $img = $(this);
-	    var imgID = $img.attr('id');
-	    var imgClass = $img.attr('class');
-	    var imgURL = $img.attr('src');
+		$('img.svg').each(function() {
+		    var $img = $(this);
+		    var imgID = $img.attr('id');
+		    var imgClass = $img.attr('class');
+		    var imgURL = $img.attr('src');
 
-	    $.get(imgURL, function(data) {
-	        // Get the SVG tag, ignore the rest
-	        var $svg = jQuery(data).find('svg');
-
-	        // Add replaced image's ID to the new SVG
-	        if(typeof imgID !== 'undefined') {
-	            $svg = $svg.attr('id', imgID);
-	        }
-	        // Add replaced image's classes to the new SVG
-	        if(typeof imgClass !== 'undefined') {
-	            $svg = $svg.attr('class', imgClass+' replaced-svg');
-	        }
-
-	        // Remove any invalid XML tags as per http://validator.w3.org
-	        $svg = $svg.removeAttr('xmlns:a');
-
-	        // Replace image with new SVG
-	        $img.replaceWith($svg);
-
-	    }, 'xml');
-
+		    $.get(imgURL, function(data) {
+		        // Get the SVG tag, ignore the rest
+		        var $svg = jQuery(data).find('svg');
+		        // Add replaced image's ID to the new SVG
+		        if(typeof imgID !== 'undefined') {
+		            $svg = $svg.attr('id', imgID);
+		        }
+		        // Add replaced image's classes to the new SVG
+		        if(typeof imgClass !== 'undefined') {
+		            $svg = $svg.attr('class', imgClass+' replaced-svg');
+		        }
+		        // Remove any invalid XML tags as per http://validator.w3.org
+		        $svg = $svg.removeAttr('xmlns:a');
+		        // Replace image with new SVG
+		        $img.replaceWith($svg);
+		    }, 'xml');
 		});
 	};
 	
-
 	// Set the date we're counting down to
-		var countDownDate = new Date("Mar 17, 2020 10:00:00").getTime();
+	var countDownDate = new Date("Mar 17, 2020 10:00:00").getTime();
 
-		// Update the count down every 1 second
-		var x = setInterval(function() {
+	// Update the count down every 1 second
+	var x = setInterval(function() {
 
-		// Get todays date and time
-		var now = new Date().getTime();
+	// Get todays date and time
+	var now = new Date().getTime();
 
-		// Find the distance between now an the count down date
-		var distance = countDownDate - now;
+	// Find the distance between now an the count down date
+	var distance = countDownDate - now;
 
-		// Time calculations for days, hours, minutes and seconds
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-		// Display the result in an element with id="demo"
-		// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
-		// + minutes + "Minutes " + seconds + "Seconds ";
+	// Display the result in an element with id="demo"
+	// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
+	// + minutes + "Minutes " + seconds + "Seconds ";
 
-		// Display the result in an element with id="demo"
-		document.getElementById("days").innerHTML = days +" <small>Tage</small>";
-		document.getElementById("hours").innerHTML = hours + " <small>Stunden</small> ";
-		document.getElementById("minutes").innerHTML = minutes + " <small>Minuten</small> ";
+	// Display the result in an element with id="demo"
+	document.getElementById("days").innerHTML = days +" <small>Tage</small>";
+	document.getElementById("hours").innerHTML = hours + " <small>Stunden</small> ";
+	document.getElementById("minutes").innerHTML = minutes + " <small>Minuten</small> ";
 //		document.getElementById("seconds").innerHTML = seconds + " <small>Sekunden</small> ";
 
-		// If the count down is finished, write some text 
-		if (distance < 0) {
-		 clearInterval(x);
-		 document.getElementById("demo").innerHTML = "Wir sind Mann & Frau!";
-		}
-		}, 1000);	
-	
-		
-	var bgVideo = function() {
-		$('.player').mb_YTPlayer();
-	};
-        
+	// If the count down is finished, write some text 
+	if (distance < 0) {
+	    clearInterval(x);
+	    document.getElementById("demo").innerHTML = "Wir sind Mann & Frau!";
+	}
 
-	// Document on load.
-	$(function(){
+}, 1000);
+			
+var bgVideo = function() {
+	$('.player').mb_YTPlayer();
+};
 
-		burgerMenu();
-		testimonialCarousel();
-		sliderMain();
-		clickMenu();
-		parallax();
-		// windowScroll();
-		navigationSection();
-		contentWayPoint();
-		inlineSVG();
-		bgVideo();
-
-
-
-
-
-	});
+// Document on load.
+$(function() {
+	burgerMenu();
+	testimonialCarousel();
+	sliderMain();
+	clickMenu();
+	parallax();
+	// windowScroll();
+	navigationSection();
+	contentWayPoint();
+	inlineSVG();
+	bgVideo();
+});
 
 
 }());
