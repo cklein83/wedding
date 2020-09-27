998d4c86bce5222aba53bd8e7a61f422d4b037c3
Christopher Klein <chris.klein@t-online.de>
2020-09-27 20:40:56 +0200

    the church is getting on focus - oh yeaaah

--- js/main.js
+++ js/main.js
@@ -224,7 +224,7 @@
 	};
 	
 	// Set the date we're counting down to
-	var countDownDate = new Date("Sep 1, 2020 10:00:00").getTime();
+	var countDownDate = new Date("Apr 17, 2021 14:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
